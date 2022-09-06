effe1ec5d7d8a4fde0f1e98c89a3790da7187076
Slb59 <sylvie.bricout2@laposte.net>
2022-09-06 11:05:16 +0200

    Creation de la fenetre de jeu

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ pygame.init()
 pygame.display.set_caption("Jeu de shooter")
 # TODO : Changer le titre du jeu et ajouter une icone de jeu
 
-pygame.display.set_mode((1080, 720))
+screen = pygame.display.set_mode((1080, 720))
 
+background = pygame.image.load('assets/bg.jpg')
 running = True
 
 # boucle de jeu
 
 while running:
+
+    # appliquer l'arriere plan
+    screen.blit(background,(0,-200))
+
+    # mettre a jour l'ecran
+    pygame.display.flip()
 
     # si le joueur ferme cette fenetre
     for event in pygame.event.get():
